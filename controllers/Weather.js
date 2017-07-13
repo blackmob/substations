616d4bc6f6b35858853e5f8c8fb650dff9caef71
Ben Blackmore <ben.blackmore@greaterheights.onmicrosoft.com>
2017-07-13 22:30:17 +0100

    prime redis and edit the service to read values from redis

--- controllers/Weather.js
+++ controllers/Weather.js
@@ -1,18 +1,18 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
+var redis_1 = require("redis");
 var weather = require('weather-js');
 exports.getWeather = function (req, res, next) {
-    // Code necessary to consume the Weather API and respond
-    weather.find({
-        search: req.swagger.params.location.value,
-        degreeType: req.swagger.params.unit.value
-    }, function (err, result) {
-        if (err) {
-            console.log(err.stack);
-            return next(err.message);
-        }
+    try {
+        var client = redis_1.createClient({ host: 'redis' });
+        client.KEYS(req.swagger.params.location.value, function (err, result) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result));
+        });
+    }
+    catch (e) {
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(result[0] || {}, null, 2));
-    });
+        res.end(JSON.stringify(e || {}, null, 2));
+    }
 };
 //# sourceMappingURL=Weather.js.map
--- controllers/Weather.ts
+++ controllers/Weather.ts
@@ -1,20 +1,21 @@
 'use strict';
 
-var weather = require('weather-js');
-
 import * as st from "swagger-tools";
 
-export const getWeather = (req, res, next) => {
-  // Code necessary to consume the Weather API and respond
-  weather.find({    
-    search: req.swagger.params.location.value,
-    degreeType: req.swagger.params.unit.value
-  }, function(err, result) {
-    if (err) {
-      console.log(err.stack);   
-      return next(err.message);
-    }    
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(result[0] || {}, null, 2));
-  });
+import {createClient} from 'redis';
+
+var weather = require('weather-js');
+
+export const getWeather = (req, res, next) => { 
+    try{
+        const client = createClient({  host: 'redis'});
+        client.KEYS(req.swagger.params.location.value, (err, result)=>{
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));
+    });
+    }catch(e) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(e || {}, null, 2));
+    }
+
 };
